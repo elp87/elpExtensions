d28460f928efbaae520ac63b89398e7e4c446994
elp87 <elp1987@gmail.com>
2013-02-01 16:36:20 +0400

    Add files

---
+++ source/ListBoxExts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace elpexts
+{
+    public static class ListBoxExts
+    {
+        /// <summary>
+        /// Возвращает значение индекса выбранного элемента
+        /// </summary>
+        public static int getSelectedIndex (this ListBox curListBox)
+        {
+            return curListBox.SelectedIndex;
+        }
+
+        /// <summary>
+        /// Возвращает коллекцию значений индексов выбранных элементов
+        /// </summary>
+        public static List<int> getSelectedIndeces(this ListBox curListBox)
+        {
+            List<int> selectedIndeces = new List<int>();
+            foreach (ListBoxItem lbItem in curListBox.Items)
+            {
+                int index = curListBox.Items.IndexOf(lbItem);
+                if (lbItem.IsSelected)
+                {
+                    selectedIndeces.Add(index);
+                }
+            }
+            return selectedIndeces;
+        }
+
+        /// <summary>
+        /// Добавляет новый item в ListBox или ListView
+        /// </summary>
+        /// <param name="curItemCollection">коллекция элементов</param>
+        /// <param name="value">Текст нового элемента</param>
+        public static void addString(this ItemCollection curItemCollection, string value)
+        {
+            ListViewItem newLBItem = new ListViewItem();
+            newLBItem.Content = value;
+            curItemCollection.Add(newLBItem);
+        }        
+    }
+}
