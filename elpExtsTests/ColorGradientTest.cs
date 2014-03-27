09a29f2902833522c7c341a72ac1c3747396a4de
elp87 <elp1987@gmail.com>
2014-03-27 15:12:46 +0400

    Add ColorGradient class

---
+++ elpExtsTests/ColorGradientTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Media;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using elp.Extensions.Colors;
+
+namespace elpExtsTests
+{
+    [TestClass]
+    public class ColorGradientTest
+    {
+        [TestMethod]
+        public void TestFromRedToYellow()
+        {
+            SolidColorBrush expRed = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+            SolidColorBrush expYellow = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+
+            Assert.AreEqual(expRed.Color, ColorGradient.FromRedToYellow(0, 100, 0).Color);
+            Assert.AreEqual(expYellow.Color, ColorGradient.FromRedToYellow(0, 100, 100).Color);
+
+            Assert.AreEqual(expRed.Color, ColorGradient.FromRedToYellow(0, 50, 0).Color);
+            Assert.AreEqual(expYellow.Color, ColorGradient.FromRedToYellow(0, 50, 50).Color);
+        }
+
+        [TestMethod]
+        public void TestFromYellowToGreen()
+        {
+            SolidColorBrush expYellow = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+            SolidColorBrush expGreen = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+
+            Assert.AreEqual(expYellow.Color, ColorGradient.FromYellowToGreen(0, 100, 0).Color);
+            Assert.AreEqual(expGreen.Color, ColorGradient.FromYellowToGreen(0, 100, 100).Color);
+
+            Assert.AreEqual(expYellow.Color, ColorGradient.FromYellowToGreen(0, 50, 0).Color);
+            Assert.AreEqual(expGreen.Color, ColorGradient.FromYellowToGreen(0, 50, 50).Color);
+        }
+
+        [TestMethod]
+        public void TestFromRedToGreen()
+        {
+            SolidColorBrush expRed = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+            SolidColorBrush expYellow = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+            SolidColorBrush expGreen = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+
+            Assert.AreEqual(expRed.Color, ColorGradient.FromRedToGreen(0, 100, 0).Color);
+            Assert.AreEqual(expYellow.Color, ColorGradient.FromRedToGreen(0, 100, 50).Color);
+            Assert.AreEqual(expGreen.Color, ColorGradient.FromRedToGreen(0, 100, 100).Color);
+        }
+    }
+}
---
+++ elpexts/ColorGradient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace elp.Extensions.Colors
+{
+    public static class ColorGradient
+    {
+        public static SolidColorBrush FromRedToGreen(double min, double max, double value)
+        {
+            Color color = new Color();
+
+            double percentageValue = ((value - min) * 100) / (max - min);
+            if (value >= min && value <= max)
+            {                
+                if (percentageValue > 50)
+                {
+                    return FromYellowToGreen(50, 100, percentageValue);
+                }
+                else
+                {
+                    return FromRedToYellow(0, 50, percentageValue);
+                }
+                
+            }
+            return Brushes.Transparent;
+        }
+
+        public static SolidColorBrush FromRedToYellow(double min, double max, double value)
+        {
+            Color color = new Color();
+
+            double percentageValue = ((value - min) * 100) / (max - min);
+
+            if (value >= min && value <= max)
+            {
+                color.A = 255;
+                color.R = 255;
+                color.B = 0;
+
+                double g = 2.55 * percentageValue;
+                color.G = (byte)Math.Round(g);
+
+                SolidColorBrush brush = new SolidColorBrush(color);
+                return brush;
+
+            }
+            return Brushes.Transparent;
+        }
+
+        public static SolidColorBrush FromYellowToGreen(double min, double max, double value)
+        {
+            Color color = new Color();
+
+            double percentageValue = ((value - min) * 100) / (max - min);
+
+            if (value >= min && value <= max)
+            {
+                color.A = 255;
+                color.G = 255;
+                color.B = 0;
+
+                double r = 255 - (2.55 * percentageValue);
+                color.R = (byte)Math.Round(r);
+
+                SolidColorBrush brush = new SolidColorBrush(color);
+                return brush;
+            }
+            return Brushes.Transparent;
+        }
+    }
+}
