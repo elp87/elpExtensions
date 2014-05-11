c5afa7304bef1a1583f81b48be9a111dd1206481
elp87 <elp1987@gmail.com>
2014-05-11 20:41:14 +0400

    Add Logger

---
+++ elpExtsTests/LoggerTest.cs
@@ -0,0 +1,34 @@
+﻿using elp87.Helpers;
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace elpExtsTests
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void TestLoggerConstructor()
+        {
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Logger log = new Logger(dir, "testLog");
+
+            Assert.AreEqual(File.Exists(log.FilePath), true);
+        }
+
+        [TestMethod]
+        public void TestWriteLine()
+        {
+            string[] lines = new string[] { "first line", "second line", "third line", "fourth line" };
+            
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Logger log = new Logger(dir, "testLogWriteLine");
+            foreach (string line in lines)
+            {
+                log.WriteLine(line);
+            }
+
+        }
+    }
+}
---
+++ elpexts/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace elp87.Helpers
+{
+    public class Logger
+    {
+        #region Fields
+        private string _fileName;
+        private string _filePath;
+        #endregion
+
+        #region Constructors
+        public Logger(string dirPath, string logName)
+        {
+            if(!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            _fileName = logName + "_" + DateTime.Now.ToString("yyMMdd-HH-mm-ss") + ".txt";
+            if (dirPath.EndsWith(@"\"))
+            {
+                _filePath = dirPath + _fileName;
+            }
+            else
+            {
+                _filePath = dirPath + @"\" + _fileName;
+            }
+            File.Create(_filePath).Close();            
+        }
+        #endregion
+
+        #region Properties
+        public string FileName { get { return _fileName; } }
+        
+        public string FilePath { get { return _filePath; } }
+        #endregion
+
+        #region Methods
+        public void WriteLine(string line)
+        {
+            using (StreamWriter file = new StreamWriter(_filePath, true))
+            {
+                file.WriteLine(line);
+            }
+        }
+        #endregion
+    }
+}
