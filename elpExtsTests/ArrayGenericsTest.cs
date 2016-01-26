549232dc2e6397dc64f836dd74037d3ba46c25dc
elp87 <elp1987@gmail.com>
2016-01-26 15:50:39 +0300

    ArrayGenerics: Add Concat method

---
+++ elpExtsTests/ArrayGenericsTest.cs
@@ -0,0 +1,35 @@
+﻿using elp87.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace elpExtsTests
+{
+    [TestClass]
+    public class ArrayGenericsTest
+    {
+        [TestMethod]
+        public void Concat_Simple()
+        {
+            int[] a = {0, 1, 2, 3};
+            int[] b = {4, 5, 6, 7};
+
+            int[] exp = {0, 1, 2, 3, 4, 5, 6, 7};
+
+            int[] c = ArrayGeneric.Concat(a, b);
+
+            CollectionAssert.AreEqual(exp, c);
+        }
+
+        [TestMethod]
+        public void Concat_SameArray()
+        {
+            int[] a = { 0, 1, 2, 3 };
+            int[] b = { 4, 5, 6, 7 };
+
+            int[] exp = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            a = ArrayGeneric.Concat(a, b);
+
+            CollectionAssert.AreEqual(exp, a);
+        }
+    }
+}
---
+++ elpexts/ArrayGenerics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace elp87.Helpers
+{
+    public static class ArrayGeneric
+    {
+        public static T[] Concat<T>(T[] baseArray, T[] additionalArray)
+        {
+            int newArrayLength = baseArray.Length + additionalArray.Length;
+            T[] newArray = new T[newArrayLength];
+
+            Array.Copy(baseArray, newArray, baseArray.Length);
+            Array.Copy(additionalArray, 0, newArray, baseArray.Length, additionalArray.Length);
+
+            return newArray;
+        }
+    }
+}
